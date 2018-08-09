bca8bc224213fb31cc0ae14378b611a4b24f2365
Romulomga <rm.abrahao@gmail.com>
2018-08-09 18:54:15 -0300

    Adding new code files

---
+++ HMRRangeSlider/Classes/HMRRangeSliderDelegate.h
@@ -0,0 +1,30 @@
+//
+//  HMRRangeSliderDelegate.h
+//  HMRRangeSlider
+//
+//  Created by Romulo Monteiro on 09/08/2018.
+//
+
+#import <Foundation/Foundation.h>
+@class HMRRangeSlider;
+
+@protocol HMRRangeSliderDelegate <NSObject>
+
+@optional
+
+/**
+ * Called when the RangeSlider values are changed
+ */
+- (void)rangeSlider:(HMRRangeSlider *)sender didChangeSelectedMinimumValue:(float)selectedMinimum andMaximumValue:(float)selectedMaximum;
+
+/**
+ * Called when the user has finished interacting with the RangeSlider
+ */
+- (void)rangeSlider:(HMRRangeSlider *)sender didEndTouchesInLeftHandle:(BOOL)leftSelected orRightHandle:(BOOL)rightSelected;
+
+/**
+ * Called when the user has started interacting with the RangeSlider
+ */
+- (void)rangeSlider:(HMRRangeSlider *)sender didStartTouchesInLeftHandle:(BOOL)leftSelected orRightHandle:(BOOL)rightSelected;
+
+@end
